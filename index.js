86ad4269c8d8ca8b656d68890a97ab93e61a4988
devnoman01 <devnoman01@gmail.com>
2022-07-01 09:06:46 +0600

    database connection successful

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const cors = require("cors");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rssh4.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("DB Connected");
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Task Management App Server Running");
